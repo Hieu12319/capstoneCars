f1971339c72f7675829e35324dd8028090e62571
Hieu <htn619@gmail.com>
2022-08-09 16:49:46 -0700

    created backend and started working with the frontend

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
 require("dotenv").config()
-
-
-const { PORT = 3001 } = process.env
-
+const { PORT = 3001, DATABASE_URL } = process.env
 const express = require("express")
-const { default: mongoose } = require("mongoose")
-
 const app = express()
+const mongoose = require("mongoose")
+const cors = require("cors")
+const morgan = require("morgan")
 
 
 
@@ -19,12 +17,62 @@ mongoose.connection
     .on("error", (error) => console.log(error))
 
 // cars models
-    
+
+const CarsSchema = new mongoose.Schema({
+    make: String,
+    model: String,
+    year: String,
+    image: String,
+    price: String,
+})
+
+const Cars = mongoose.model("Cars", CarsSchema)
+
+//middleware
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json)
 
 //routes
     
 app.get("/", (req, res) => {
-    res.send("hello World")
+    res.send("Sell Your Cars!!")
+})
+// index cars
+app.get("/cars", async (req, res) => {
+    try{
+        res.json(await Cars.find({}))
+    } catch (error) {
+        res.status(400).json(error)
+    }
 })
 
+//cars create
+app.post("/cars", async (req, res) => {
+    try {
+        res.json(await Cars.create(req.body))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+//cars delete
+app.delete("/cars/:id", async (req, res) => {
+    try{
+        res.json(await Cars.findByIdAndDelete(req.params.id))
+    }catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+//cars update
+app.put("cars/:id", async (req, res) => {
+    try {
+        res.json(
+            await Cars.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        )
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`))
